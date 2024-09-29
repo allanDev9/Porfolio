ec9404da1df3dc82b69171d9a7da219b9428d1c4
allanDev9 <allanpenaguerra2003@gmail.com>
2024-09-28 22:02:02 -0600

    aniamtion porfolio , use javascript with css for animation

---
+++ index.js
@@ -0,0 +1,13 @@
+let modalElement = document.getElementById('modal')
+
+function openModal(){
+    modalElement.classList.add('open-modal')
+    modalElement.classList.remove('close-modal')
+}
+function closeModal(){
+    modalElement.classList.remove('close-modal')
+    setTimeout(() => {
+        modalElement.classList.remove('open-modal');
+        
+    }, 400);
+}
